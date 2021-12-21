9f2ca5a2669c1f21f88749ae8d5b198c2373979d
minTwin <michaelmin2020@gmail.com>
2021-12-21 16:54:17 -0600

    commit
    Added initial starting files

---
+++ FirstWebAPI3/FirstWebAPI3/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebAPI3.Controllers
+{
+    // api/hello
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+
+
+    }
+
+}
